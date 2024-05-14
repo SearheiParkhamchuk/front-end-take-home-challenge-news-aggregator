95c172fe397b10e93dbe144cc3798a3f9da702c4
Siarhei Parkhamchuk <s.parkhamchuk@gmail.com>
2024-05-15 00:14:51 +0200

    FEAT: Add another new source `fake-new`. For `development` only

---
+++ src/05-entities/articles/api/fake-news-source/get-many/client-api/request.ts
@@ -0,0 +1,16 @@
+import { getFetcherInstance } from '@/06-shared/lib/third-party/fetcher/get-fetcher-instance'
+
+import { type ArticleSerializedResponseQueryMany, type ArticlesQueryParams } from '../../../@types'
+
+export const fakeNewsArticlesClientApiRequest = async (
+  params: ArticlesQueryParams,
+  options?: { signal: AbortSignal }
+) => {
+  const response = await getFetcherInstance().request<ArticleSerializedResponseQueryMany>({
+    method: 'GET',
+    url: '/api/articles/fake-news-source',
+    params,
+    signal: options?.signal
+  })
+  return response.data
+}
