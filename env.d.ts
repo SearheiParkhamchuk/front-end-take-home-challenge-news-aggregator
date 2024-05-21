4986029e67dcb66fc66a5fb83badeb86155cc2d5
Siarhei Parkhamchuk <s.parkhamchuk@gmail.com>
2024-05-21 20:13:36 +0200

    FEAT: Initial server config

--- env.d.ts
+++ env.d.ts
@@ -1,8 +1,10 @@
 declare namespace NodeJS {
   interface ProcessEnv {
-    API_KEY_NEWS_API: string
-    API_KEY_NEW_YORK_TIMES_ARTICLES: string
-    API_KEY_THE_GUARDIAN: string
+    CACHE_DB_NAME: string
+    CACHE_DB_PASSWORD: string
+    CACHE_DB_PORT: number
+    CACHE_DB_USERNAME: string
+
     NEXT_PUBLIC_APP_BASE_URL: string
   }
 }
