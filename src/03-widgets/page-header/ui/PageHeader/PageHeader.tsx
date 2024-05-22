c1c7a3405e0f2cc219f632ea2d7e0c1328ddc65a
Siarhei Parkhamchuk <s.parkhamchuk@gmail.com>
2024-05-22 20:08:59 +0200

    IMPROVEMENT: Move `ArticlesSearch` from the page header panel into  `ArticlesTopPanel`

--- src/03-widgets/page-header/ui/PageHeader/PageHeader.tsx
+++ src/03-widgets/page-header/ui/PageHeader/PageHeader.tsx
@@ -1,4 +1,3 @@
-import ArticlesSearch from '@/04-features/articles-search/ui/ArticlesSearch'
 import Logo from '@/05-entities/app/ui/Logo'
 
 import styles from './styles.module.scss'
@@ -7,9 +6,6 @@ function PageHeader() {
   return (
     <div className={styles.inner}>
       <Logo />
-      <div className={styles['search-container']}>
-        <ArticlesSearch />
-      </div>
     </div>
   )
 }
