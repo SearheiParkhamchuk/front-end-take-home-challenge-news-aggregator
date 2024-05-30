40dff279cf9ae4926f0e6e9ab49f19cc50ac45d2
Siarhei Parkhamchuk <s.parkhamchuk@gmail.com>
2024-05-30 22:25:26 +0200

    REFACTORING: Reworked `ArticleLayout` styles

--- src/06-shared/ui/Stack/types.ts
+++ src/06-shared/ui/Stack/types.ts
@@ -1,5 +1,5 @@
-import { type StackProps as MantineStackProps } from '@mantine/core'
+import { type MantineSpacing, type StackProps as MantineStackProps } from '@mantine/core'
 
 export type StackProps = Pick<
-MantineStackProps, 'children' | 'className' | 'gap' | 'align' | 'justify'
->
+MantineStackProps, 'children' | 'className' | 'align' | 'justify'
+> & { gap?: MantineSpacing | 'none' }
