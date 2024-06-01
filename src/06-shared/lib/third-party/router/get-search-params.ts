5585ed50614d30d4c94f961928f475f37983bff1
Siarhei Parkhamchuk <s.parkhamchuk@gmail.com>
2024-06-01 23:16:04 +0200

    REFACTORING: getSearchParams => getUrlWithSearchParams

--- src/06-shared/lib/third-party/router/get-search-params.ts
+++
@@ -1,6 +0,0 @@
-import { mergeSearchParams } from './merge-search-params'
-
-export function getSearchParams(params: Record<string, string> | URLSearchParams) {
-  const searchParams = mergeSearchParams([new URLSearchParams(window.location.search), new URLSearchParams(params)])
-  return new URL(`?${searchParams.toString()}`, window.location.href)
-}
