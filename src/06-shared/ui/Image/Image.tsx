defa906964bf12021969525935f572c90a656ccd
Siarhei Parkhamchuk <s.parkhamchuk@gmail.com>
2024-06-18 18:07:37 +0200

    IMPROVEMENT[shared][ui]: Added `width` and `heigth` properties to `Image` component

--- src/06-shared/ui/Image/Image.tsx
+++ src/06-shared/ui/Image/Image.tsx
@@ -3,10 +3,10 @@ import { IconPhotoOff } from '@tabler/icons-react'
 import styles from './styles.module.scss'
 import { type ImageProps } from './types'
 
-function Image({ src, alt, className = '', withBackground, ...rest }: ImageProps) {
+function Image({ src, alt, height = '100%', width = '100%', className = '', withBackground }: ImageProps) {
   if (!src) {
     return (
-      <div className={`${styles['no-image-plug']} ${withBackground ? styles['image-bg'] : ''} ${className}`} {...rest}>
+      <div className={`${styles['no-image-plug']} ${withBackground ? styles['image-bg'] : ''} ${className}`}>
         <IconPhotoOff style={{ width: '4rem', height: '4rem' }} />
       </div>
     )
@@ -15,11 +15,12 @@ function Image({ src, alt, className = '', withBackground, ...rest }: ImageProps
   return (
     // eslint-disable-next-line @next/next/no-img-element
     <img
-      {...rest}
       alt={alt}
-      className={`${className} ${styles.image} ${withBackground ? styles['image-bg'] : ''}`}
+      className={`${className} ${withBackground ? styles['image-bg'] : ''}`}
+      height={height}
       loading='lazy'
       src={src}
+      width={width}
     />
   )
 }
